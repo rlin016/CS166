f160bd4bc0afdc7d37facb73bf89aae242d1be3d
Raquel Lin <45045419+rlin016@users.noreply.github.com>
2021-06-09 15:12:28 -0700

    Added "(space)" to patient name check
    Added ' ' to the search array in isCharInput (patient name check function)

--- code/java/src/DBproject.java
+++ code/java/src/DBproject.java
@@ -474,7 +474,7 @@ public class DBproject{
 	}
 	public static boolean isCharInput(String input){
 		input = input.toLowerCase();
-		char[] search = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		char[] search = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
 		int count = 0;
 		for(int i = 0; i < input.length(); i++){
 			char x = input.charAt(i);
