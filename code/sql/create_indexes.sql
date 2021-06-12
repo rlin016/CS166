5c55a060a6df27a496a753065f833cb28ad4e1c0
Raquel Lin <rlin016@wch132-35.cs.ucr.edu>
2021-06-11 18:43:11 -0700

    indexes

---
+++ code/sql/create_indexes.sql
@@ -0,0 +1,31 @@
+create index patient_ID_index
+on patient (patient_ID, name);
+
+create index hospital_ID_index
+on hospital (hospital_ID, name);
+
+create index department_ID_index
+on department (dept_ID, hid);
+
+create index staff_ID_index
+on staff (staff_ID, hid);
+
+create index doctor_ID_index
+on doctor (doctor_ID, name, did);
+
+create index appointment_ID_index
+on appointment(appnt_ID, status);
+
+create index request_maintenance_index
+on request_maintenance (did, sid, dept_name);
+
+create index searches_index
+on searches (hid, pid, aid);
+
+create index schedules_index
+on schedules (appt_id, staff_id);
+
+create index has_appointment
+on has_appointment (appt_id, doctor_id);
+
+
